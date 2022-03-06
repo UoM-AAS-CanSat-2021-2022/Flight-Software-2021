7ce307715d8c4b82092ac7497b3f3beb28b68ec7
Sam <tritoke@protonmail.com>
2022-03-06 01:19:00 +0000

    testing.cpp and main.cpp now both compile with C++ 17

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,36 @@
 #include <Arduino.h>
 
+const int led = LED_BUILTIN;
+
 void setup() {
+	pinMode(led, OUTPUT);
 	Serial.begin(9600);
-	//Serial.setTimeout(100);
+}
+
+struct ret_type {
+	bool a;
+	unsigned int b;
+};
+
+ret_type get_val() {
+	static unsigned int x = 42;
+
+	x ^= x << 17;
+	x ^= x >> 13;
+	x ^= x << 5;
+
+	return {(x & 1) == 0, x};
 }
 
 void loop() {
-	// do something I guess
+	auto [x, y] = get_val();
+
+	Serial.print("x: ");
+	Serial.print(x);
+	Serial.print(", y: ");
+	Serial.println(y, HEX);
+
+	delay(1000);
+	digitalWrite(led, x ? HIGH : LOW);
 }
 
