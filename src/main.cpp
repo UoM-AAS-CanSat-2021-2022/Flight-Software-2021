8f17211e1da0e8c4f22197301921d083596ba1e6
Sam <tritoke@protonmail.com>
2022-04-05 17:52:34 +0100

    Initial tests with AT mode.

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,27 @@
 
 constexpr static int led = LED_BUILTIN;
 SoftwareSerial XBeeSerial { PIN_SERIAL_RX, PIN_SERIAL_TX };
-XBee xbee {};
+//XBee xbee {};
 
 void setup() {
 	pinMode(led, OUTPUT);
 	Serial.begin(9600);
 	XBeeSerial.begin(9600);
-	xbee.setSerial(XBeeSerial);
+	//xbee.setSerial(XBeeSerial);
 }
 
 void loop() {
-	static std::uint8_t payload[] { "beans" };
-	static Tx16Request tx { 1057, payload, sizeof(payload) };
-	xbee.send(tx);
-	delay(1000);
+	//static std::uint8_t payload[] { "beans" };
+	//static Tx16Request tx { 1057, payload, sizeof(payload) };
+	//xbee.send(tx);
+
+	while (Serial.available()) {
+		XBeeSerial.write(Serial.read());
+	}
+
+	while (XBeeSerial.available()) {
+		Serial.write(XBeeSerial.read());
+	}
+
+	delay(10);
 }
