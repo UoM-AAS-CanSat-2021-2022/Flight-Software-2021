568a5f21ab07ae34aed17ba52e7272c60a2ebd34
GeorgeTheof2002 <georgewolftheof2002@gmail.com>
2022-04-05 15:21:14 +0100

    Initial BMP commit.

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,51 @@
 #include <Arduino.h>
-#include "util/sout.hpp"
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
 
-const int led = LED_BUILTIN;
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BMP3XX bmp;
 
 void setup() {
-	pinMode(led, OUTPUT);
-	Serial.begin(9600);
-}
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Adafruit BMP388 / BMP390 test");
 
-struct ret_type {
-	bool a;
-	unsigned int b;
-};
+  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+    //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode
+    //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
+  }
 
-ret_type get_val() {
-	static unsigned int x = 42;
-
-	x ^= x << 17;
-	x ^= x >> 13;
-	x ^= x << 5;
-
-	return {(x & 1) == 0, x};
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 }
 
 void loop() {
-	auto [x, y] = get_val();
+  if (! bmp.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bmp.temperature);
+  Serial.println(" *C");
 
-	sout << "x: " << x
-	     << ", y: " << std::hex << y
-	     << std::dec
-		 << std::endl;
+  //Serial.print("Pressure = ");
+  //Serial.print(bmp.pressure / 100.0);
+  //Serial.println(" hPa");
 
-	delay(300);
-	digitalWrite(led, x ? HIGH : LOW);
+  //Serial.print("Approx. Altitude = ");
+ // Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+ // Serial.println(" m");
 }
