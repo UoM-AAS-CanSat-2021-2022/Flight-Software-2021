620f6b90a30864c0b9091b0a6f40c7130adfa7b3
Sam <tritoke@protonmail.com>
2022-04-07 04:08:02 +0100

    Got a reading example working.

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,44 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <XBee.h>
+#include <cstdint>
+#include <iomanip>
 #include "util/sout.hpp"
 
 constexpr static int led = LED_BUILTIN;
 SoftwareSerial XBeeSerial { PIN_SERIAL_RX, PIN_SERIAL_TX };
-//XBee xbee {};
+XBee xbee {};
 
 void setup() {
 	pinMode(led, OUTPUT);
+
 	Serial.begin(9600);
 	XBeeSerial.begin(9600);
-	//xbee.setSerial(XBeeSerial);
+	xbee.setSerial(XBeeSerial);
 }
 
 void loop() {
-	//static std::uint8_t payload[] { "beans" };
-	//static Tx16Request tx { 1057, payload, sizeof(payload) };
-	//xbee.send(tx);
+	static Rx16Response rx {};
 
-	while (Serial.available()) {
-		XBeeSerial.write(Serial.read());
-	}
+    xbee.readPacket();
+    
+    if (xbee.getResponse().isAvailable()) {
+		sout << "Got response." << std::endl;
+		
+		if (xbee.getResponse().getApiId() == RX_16_RESPONSE) {
+			xbee.getResponse().getRx16Response(rx);
 
-	while (XBeeSerial.available()) {
-		Serial.write(XBeeSerial.read());
-	}
-
-	delay(10);
+			std::string_view rx_data { (char*) rx.getData(), (size_t) rx.getDataLength() };
+			sout << "Received data: " << std::quoted(rx_data) << " - ";
+			for (auto i = 0; i < rx.getDataLength(); i++) {
+				sout << std::hex << +rx.getData()[i] << ' ';
+			}
+			sout << std::endl;
+		} else {
+			// not something we were expecting
+			sout << "Got something unexpected - API ID = " << std::hex << +xbee.getResponse().getApiId() << std::endl;
+		}
+    } else if (xbee.getResponse().isError()) {
+		sout << "Error reading packet.  Error code: " << std::hex << +xbee.getResponse().getErrorCode() << std::endl;
+    }
 }
