ee49c9bb587b7d335160dd01a2cba033345524d3
Sam <tritoke@protonmail.com>
2022-04-13 19:09:46 +0100

    Created a class to manage communications with the XBee.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
-#include "util/sout.hpp"
 #include "xbee/manager.hpp"
+#include "util/sout.hpp"
 
 XBeeManager xbm;
 
 void setup() {
-	Serial.begin(230400);
+	Serial.begin(9600);
 	xbm.setup(Serial1);
+	xbm.set_panid(6057);
 }
 
 void loop() {
-	static bool ran = false;
-	if (!ran) {
-		xbm.set_panid(0x6969);
-		ran = true;
+	static std::string message = "Hi there!";
+	static auto last_sent = 0;
+	if ((millis() - last_sent) > 1000) {
+		xbm.send(message);
+		last_sent = millis();
 	}
+
+	xbm.loop();
 }
--- src/xbee/manager.hpp
+++ src/xbee/manager.hpp
@@ -1,11 +1,16 @@
 #include <cstdint>
+#include <string>
 #include <XBee.h>
 
 class XBeeManager {
     XBeeWithCallbacks _xbee;
+    std::uint16_t _panid;
 
 public:
-    // XBeeManager();
+    XBeeManager();
+    void loop();
     void setup(HardwareSerial&);
-    void set_panid(std::uint16_t);
+    std::uint8_t set_panid(std::uint16_t, std::uint16_t timeout=100);
+    std::uint16_t get_panid() const;
+    void send(std::string&);
 };
